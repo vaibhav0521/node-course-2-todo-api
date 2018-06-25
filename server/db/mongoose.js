5daf810d31f5ad8265cdf67e6a87e1cd35e84eac
vaibhav <vaibhavagarwal606@gmail.com>
2018-06-26 01:13:43 +0530

    error solve

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://vaibhav4205:frnd4ever@ds117681.mlab.com:17681/mydb' || 'mongodb://localhost:27017/TodoApp');
+mongoose.connect(process.env.MONGODB_URI);
 
 module.exports = {mongoose};
